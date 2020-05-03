d51322c15a3c91347694d03837ef5c8a0326651b
Ravi Prakash <raviskit2012@gmail.com>
2020-05-03 18:22:43 +0530

    Added icalender gem and updated to_ical method

--- cm_challenge/absences.rb
+++ cm_challenge/absences.rb
@@ -1,9 +1,43 @@
 require_relative './api'
+require 'icalendar'
 
 module CmChallenge
   class Absences
     def to_ical
-      # Your implementation here
+      @absences = CmChallenge::Api.absences
+      @members = CmChallenge::Api.members
+      cal = Icalendar::Calendar.new
+
+      @absences.each do |absence|
+        member = @members.detect { |member| member[:user_id] == absence[:user_id] }
+        cal.event do |e|
+          e.dtstart     = Icalendar::Values::Date.new(absence[:start_date].gsub('-', ''))
+          e.dtend       = Icalendar::Values::Date.new(absence[:end_date].gsub('-', ''))
+          e.summary     = absence_type(absence[:type], member[:name])
+          e.description = absence_description(absence[:member_note], member[:name])
+          e.ip_class    = "PRIVATE"
+        end
+      end
+
+      cal.publish
+      cal
+    end
+
+    private
+
+    def absence_description(note, name)
+      note.empty? ? "" : "#{name}'s note: #{note}"
+    end
+
+    def absence_type(type, name)
+      case type
+      when "vacation"
+        "#{name} is on vacation"
+      when "sickness"
+        "#{name} is sick"
+      else
+        "#{name} is absent"
+      end
     end
   end
 end
