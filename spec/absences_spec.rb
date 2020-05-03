2c43b113cfd043aaa994b1f52068b9e998b52dee
Ravi Prakash <raviskit2012@gmail.com>
2020-05-03 18:28:02 +0530

    added specs for absences

--- spec/absences_spec.rb
+++ spec/absences_spec.rb
@@ -1,5 +1,20 @@
 require_relative '../cm_challenge/absences'
+require 'icalendar'
 
 RSpec.describe 'Absences' do
-  it { skip 'Your tests here' }
+  describe "#to_ical" do
+
+    subject { CmChallenge::Absences.new.to_ical }
+
+    it "lists all the events" do
+      expect(subject.events.count).to eq(42)
+    end
+
+    it "shows correct data" do
+      expect(subject.events.first.summary).to eq("Mike is sick")
+      expect(subject.events.first.description).to eq("")
+      expect(subject.events.first.dtstart.to_s).to eq("2017-01-13")
+      expect(subject.events.first.dtend.to_s).to eq("2017-01-13")
+    end
+  end
 end
